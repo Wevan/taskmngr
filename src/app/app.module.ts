a8e60fa7bce110b06cc5d4ef428e795d4ee777e4
Wevan <438864121@qq.com>
2018-12-16 21:12:26 +0800

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {CoreModule} from './core/core.module';
+import {LoginModule} from './login/login.module';
+import { SearchComponent } from './search/search.component';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  declarations: [AppComponent, SearchComponent],
+  imports: [BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,30 @@
+import {NgModule, SkipSelf, Optional} from '@angular/core';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ShareModule} from '../share/share.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ],
+  imports: [
+    ShareModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
+  exports: [HeaderComponent, FooterComponent, SidebarComponent]
+})
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parent: CoreModule) {
+
+    if (parent) {
+      throw new Error('模块已经存在！');
+    }
+  }
+}
